68611872732e79c7f11f07cabe5f78d99d37bc2b
anmv <anmv@itu.dk>
2024-10-24 10:09:52 +0200

    Comments in main

--- EfficientRoutePlanning/app/src/main/java/Project/Main/Main.java
+++ EfficientRoutePlanning/app/src/main/java/Project/Main/Main.java
@@ -46,12 +46,12 @@ public class Main {
         // String filePath = "Efficient Route Planning/app/src/main/resources/denmark.graph";
         // GraphBuilderResult result = GraphBuilder.buildGraphFromFile(filePath);
 
-        InputStream inputStream = Main.class.getResourceAsStream("/denmark.graph");
-        if (inputStream == null) {
-            throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
-        }
-        GraphBuilderResult graphResult = GraphBuilder.buildGraphFromInputStream(inputStream);
-        System.out.println(graphResult.getLongIntegerMap().size());
+    //     InputStream inputStream = Main.class.getResourceAsStream("/denmark.graph");
+    //     if (inputStream == null) {
+    //         throw new FileNotFoundException("Resource 'denmark.graph' not found in classpath");
+    //     }
+    //     GraphBuilderResult graphResult = GraphBuilder.buildGraphFromInputStream(inputStream);
+    //     System.out.println(graphResult.getLongIntegerMap().size());
     }
 
 
